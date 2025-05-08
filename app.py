499ec0853635dc64f83ee4c8b59995a5860f0899
ManuelParrado <manuparradobarrero2004@gmail.com>
2025-05-08 21:45:49 +0200

    changes

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.markdown("Sube una imagen y el modelo la clasificará como una de las siguien
 st.write(", ".join(classes))
 
 # Subida de imagen
-uploaded_file = st.file_uploader("Sube una imagen...", type=["jpg", "jpeg", "png", "JPG", "JPEG", "PNG"])
+uploaded_file = st.file_uploader("Sube una imagen...", type=None)
 
 
 if uploaded_file is not None:
