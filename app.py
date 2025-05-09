89c586d8c483a1383e9609ee589a7b42d4a38fa8
ManuelParrado <manuparradobarrero2004@gmail.com>
2025-05-09 21:52:30 +0200

    changes

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 
 # Cargar el modelo
-model = load_model('images_clasifier.h5')
+first_model = load_model('first_images_clasifier.h5')
+second_model = load_model('second_images_clasifier.h5')
 
 # Clases del modelo
 classes = ['Alicates', 'Cuchillo', 'Cúter', 'Destornillador', 'Martillo', 'Tijeras']
@@ -26,12 +27,21 @@ if uploaded_file is not None:
     img = load_img(uploaded_file, target_size=(150, 150))
     img_array = img_to_array(img) / 255.0
     img_array = np.expand_dims(img_array, axis=0)
+    
+ # ----------- Primer modelo -----------
+    first_predictions = first_model.predict(img_array)
+    first_predicted_index = np.argmax(first_predictions)
+    first_predicted_class = classes[first_predicted_index]
 
-    # Predicción
-    predictions = model.predict(img_array)
-    predicted_index = np.argmax(predictions)
-    predicted_class = classes[predicted_index]
+    st.subheader("Predicción del Primer Modelo")
+    st.write(f"Clase predicha: **{first_predicted_class}**")
+    st.bar_chart({clase: float(prob) for clase, prob in zip(classes, first_predictions[0])})
 
-    # Mostrar resultado
-    st.subheader(f"Predicción: {predicted_class}")
-    st.bar_chart({clase: float(prob) for clase, prob in zip(classes, predictions[0])})
+    # ----------- Segundo modelo -----------
+    second_predictions = second_model.predict(img_array)
+    second_predicted_index = np.argmax(second_predictions)
+    second_predicted_class = classes[second_predicted_index]
+
+    st.subheader("Predicción del Segundo Modelo")
+    st.write(f"Clase predicha: **{second_predicted_class}**")
+    st.bar_chart({clase: float(prob) for clase, prob in zip(classes, second_predictions[0])})
